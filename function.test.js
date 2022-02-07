0b0b956e77d12e02d60e66b8ee99f961e73e1491
ViktoriaB <v.chamborant@hotmail.com>
2022-02-07 15:20:54 -0600

    Finished extra challenge

--- function.test.js
+++ function.test.js
@@ -1,5 +1,5 @@
 
-const { returnTwo, greeting, add} = require('./function')
+const { returnTwo, greeting, add, multiply, divide, subtract} = require('./function')
 
  test('return should return two', () =>{
      expect(returnTwo()).toEqual(2)
@@ -10,7 +10,28 @@ const { returnTwo, greeting, add} = require('./function')
      expect(greeting('Jill')).toEqual('Hello, Jill')
  })
 
+
+
+ describe('Math functions', () =>{
+
+
  test('add test', () =>{
     expect(add(1, 2)).toEqual(3)
     expect(add(5, 9)).toEqual(14)
  })
+
+ test('multiply test', () =>{
+     expect(multiply(1, 1)).toEqual(1)
+     expect(multiply(5, 5)).toEqual(25)
+ })
+
+ test('divide test', () =>{
+    expect(divide(11, 11)).toEqual(1)
+    expect(divide(100, 2)).toEqual(50)
+})
+
+test('subtract test', () =>{
+    expect(subtract(200, 200)).toEqual(0)
+    expect(subtract(10, 2)).toEqual(8)
+}) 
+})
